958a54827853bd6148c53b61fe0a8286a1a2903b
Jessica Aguiar <jessicaag.rs@gmail.com>
2025-04-24 07:50:28 -0300

    feat: update layout and styling for access and workspace pages
    ✔️ Remove unused font imports from layout components
    ✔️ Simplify layout return structure for access and workspace pages
    ✔️ Enhance home page with logo, title, and navigation links
    ✔️ Add new color variables and update body styles in globals.css
    ✔️ Modify prepare script in package.json for better build process

--- src/app/(workspace)/layout.tsx
+++ src/app/(workspace)/layout.tsx
@@ -1,20 +1,5 @@
 import type { Metadata } from 'next';
-import { Poppins, Roboto } from 'next/font/google';
 import '../../styles/globals.css';
-
-const poppins = Poppins({
-  subsets: ['latin'],
-  variable: '--font-poppins',
-  display: 'swap',
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
-});
-
-const roboto = Roboto({
-  subsets: ['latin'],
-  variable: '--font-roboto',
-  display: 'swap',
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
-});
 
 export const metadata: Metadata = {
   title: 'Dashboard',
@@ -26,11 +11,5 @@ export default function WorkspaceLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <html lang="pt-Br">
-      <body className={`${poppins.variable} ${roboto.variable} antialiased`}>
-        {children}
-      </body>
-    </html>
-  );
+  return <>{children}</>;
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,43 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
 export default function Home() {
-  return <main>Hello World</main>;
+  return (
+    <main>
+      <div className="grid grid-cols-1 grid-rows-3 justify-items-center h-screen max-w-3xl m-(--margin-auto) bg-(--color-100) text-(--color-800)">
+        <div className="flex items-center gap-5 self-end">
+          <Image
+            src="/logo.svg"
+            alt="Logo"
+            className="sm:w-[100px] sm:h-[100px]"
+            width={50}
+            height={50}
+          />
+          <h1 className="text-3xl font-bold sm:text-6xl">CollegeWeb</h1>
+        </div>
+        <div
+          className="flex flex-col items-center gap-5 self-center"
+          aria-label="Acesso ao sistema"
+        >
+          <Link
+            href="/login"
+            className="button px-1 py-3 sm:px-3 sm:py-4 bg-(--color-200) text-(--color-1300) rounded-xl w-40 text-center hover:bg-(--color-1200) transition-all duration-200"
+          >
+            Login
+          </Link>
+          <Link
+            href="/register"
+            className="px-1 py-3 sm:px-3 sm:py-4 bg-(--color-200) text-(--color-1300) rounded-xl w-40 text-center hover:bg-(--color-1200) transition-all duration-200       "
+          >
+            Cadastre-se
+          </Link>
+        </div>
+        <footer className="self-end mb-5">
+          <p className="text-xs">
+            © 2025 @jessagrs. Todos os direitos reservados.
+          </p>
+        </footer>
+      </div>
+    </main>
+  );
 }
